ca1080f4f4e27feba043a183c96136e6c4f360df
Luigi <luigismilzo@live.com>
2020-04-18 18:29:06 +0200

    Added send message functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-    var msgInput = $('.chat-footer input');
 
+    // REFERENCES
+    var msgInput = $('.chat-footer input');
+    var chatWindow = $('.chat-main');
+
+
+    // EVENTS
+    // Send message Input
     msgInput.focusin(function () {
         $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
     });
@@ -9,16 +15,16 @@ $(document).ready(function () {
         $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
     });
 
-
-
-
-
-
-
-
-
-
-
-
+    msgInput.keyup(function(e) {
+        
+        if (msgInput.val() != '') {
+            if (e.which == 13 || e.keycode == 13) {
+                var item = $('.template .message.sent').clone();
+                item.children('p').append(msgInput.val());
+                chatWindow.append(item);
+                msgInput.val('');
+            }
+        }
+    });
 
 }); // <-- End ready
