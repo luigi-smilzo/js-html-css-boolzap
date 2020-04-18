8307a91dfd068be379ff4a359c43e19b2949429e
Luigi <luigismilzo@live.com>
2020-04-18 16:19:20 +0200

    Send message focus icon toggle

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    var msgInput = $('.chat-footer input');
+
+    msgInput.focusin(function () {
+        $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
+    });
+
+    msgInput.focusout(function () {
+        $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
+    });
 
 
 
