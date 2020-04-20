791f8289f91f298374fc426873ea15e3223a2e0a
Luigi <luigismilzo@live.com>
2020-04-20 16:45:57 +0200

    Refactoring + Autoreply

--- js/main.js
+++ js/main.js
@@ -2,29 +2,62 @@ $(document).ready(function () {
 
     // REFERENCES
     var msgInput = $('.chat-footer input');
-    var chatWindow = $('.chat-main');
-
+    var sendIcon = $('.chat-footer .send-icons');
 
     // EVENTS
     // Send message Input
-    msgInput.focusin(function () {
+    msgInput.on('focus blur', function () {
         $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
     });
 
-    msgInput.focusout(function () {
-        $(this).next().children('i').toggleClass('fa-microphone fa-paper-plane');
+    sendIcon.click(function() {
+        sendMsg(msgInput);
+        setTimeout(autoReply, 1000);
     });
 
-    msgInput.keyup(function(e) {
-        
-        if (msgInput.val() != '') {
-            if (e.which == 13 || e.keycode == 13) {
-                var item = $('.template .message.sent').clone();
-                item.children('p').append(msgInput.val());
-                chatWindow.append(item);
-                msgInput.val('');
-            }
+    msgInput.keypress(function(e) {
+        if (e.which == 13) {
+            sendMsg(msgInput);
+            setTimeout(autoReply, 1000);
         }
     });
 
 }); // <-- End ready
+
+// FUNCTIONS
+function sendMsg (input) {
+    var msgContent = input.val().trim();
+
+    if (msgContent.length > 0) {
+        var newMsg = $('.template .message').clone();
+
+        newMsg.children('p').text(msgContent);
+        timeStamp(newMsg);
+        newMsg.addClass('sent');
+        $('.chat-main-conversation.active').append(newMsg);
+        input.val('');
+    }
+}
+
+function autoReply () {
+    var newMsg = $('.template .message').clone();
+    newMsg.children('p').text('Ok');
+    timeStamp(newMsg);
+    newMsg.addClass('received');
+    $('.chat-main-conversation.active').append(newMsg);
+}
+
+function timeStamp (newMsg) {
+    var date = new Date();
+    var hour = leadZero(date.getHours());
+    var minutes = leadZero(date.getMinutes());
+    var timeStamp = hour + ':' + minutes;
+    newMsg.children('.timestamp').text(timeStamp);
+}
+
+function leadZero(param) {
+    if(param < 10) {
+        param = '0' + param;
+    }
+    return param;
+}
